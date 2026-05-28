143868f49293a7c90ab6735cc50e51852a70cd4d
osnat <obaravin@g.jct.ac.il>
2026-05-28 14:59:39 +0300

    8 part 1  tests

--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -11,6 +11,11 @@ public class Material {
     /** Shininess factor */
     public int nShininess = 0;
 
+    /** Transparency (Transmission) coefficient */
+    public Double3 kT = Double3.ZERO;
+    /** Reflection coefficient */
+    public Double3 kR = Double3.ZERO;
+
     /**
      * Setter for kD (Double3) - Method Chaining
      * @param kD diffuse coefficient
@@ -60,9 +65,49 @@ public class Material {
         this.nShininess = nShininess;
         return this;
     }
+
+    /**
+     * Setter for kT (Double3) - Transparency - Method Chaining
+     * @param kT transparency coefficient
+     * @return material object
+     */
+    public Material setKt(Double3 kT) {
+        this.kT = kT;
+        return this;
+    }
+
+    /**
+     * Setter for kT (double) - Transparency - Method Chaining
+     * @param kT transparency coefficient
+     * @return material object
+     */
+    public Material setKt(double kT) {
+        this.kT = new Double3(kT);
+        return this;
+    }
+
+    /**
+     * Setter for kR (Double3) - Reflection - Method Chaining
+     * @param kR reflection coefficient
+     * @return material object
+     */
+    public Material setKr(Double3 kR) {
+        this.kR = kR;
+        return this;
+    }
+
+    /**
+     * Setter for kR (double) - Reflection - Method Chaining
+     * @param kR reflection coefficient
+     * @return material object
+     */
+    public Material setKr(double kR) {
+        this.kR = new Double3(kR);
+        return this;
+    }
+
     /* --- Backwards Compatibility for Tests & Loader --- */
     // We add these but don't save the value to keep the class "clean" as per instructions
     public Material setKa(Double3 ka) { return this; }
     public Material setKa(double ka) { return this; }
-
 }
